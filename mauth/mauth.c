d4de0132b8855cc37da33e0b8a97a0a0990be5aa
Albert Chu <chu11@llnl.gov>
2003-12-12 19:22:27 +0000

    Output more detailed munge_decode error

--- mauth/mauth.c
+++ mauth/mauth.c
@@ -253,7 +253,8 @@ int mauth(struct mauth *ma, int fd, int cport) {
     if ((rv = munge_decode(mbuf, 0, (void **)&mptr, &buf_length, 
                            &ma->uid, &ma->gid)) != EMUNGE_SUCCESS) {
         syslog(LOG_ERR, "%s: %s", "munge_decode error", munge_strerror(rv));
-        snprintf(ma->errmsg, MAXERRMSGLEN, "Authentication Failure");
+        snprintf(ma->errmsg, MAXERRMSGLEN, "Munge Authentication Failure: %s",
+                 munge_strerror(rv));
         return -1;
     }
   
